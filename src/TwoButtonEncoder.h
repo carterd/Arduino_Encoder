3d56584734a38390c02e1f18fd875102ad66eeee
Derek Carter <carter_derek@live.com>
2023-03-12 14:25:06 +0000

    Quick fix to include file for two button encoder

--- src/TwoButtonEncoder.h
+++ src/TwoButtonEncoder.h
@@ -6,11 +6,9 @@
 class TwoButtonEncoder : public ButtonEncoder {
 public:
     TwoButtonEncoder(pin_size_t leftButtonPin, pin_size_t rightButtonPin, PinStatus pressedPinStatus, PinMode buttonPinMode);
-
-    virtual void update();
+    virtual ~TwoButtonEncoder() {}    
 protected:
     virtual void readButtons();
-    virtual ~TwoButtonEncoder() {}
 private:
     enum class DebounceState { NO_BUTTON_PRESSED, NO_TO_LEFT_BUTTON, NO_TO_RIGHT_BUTTON, ENTER_BUTTON, ENTER_TO_LEFT_BUTTON, ENTER_TO_RIGHT_BUTTON };
     DebounceState mDebounceState = DebounceState::NO_BUTTON_PRESSED;
